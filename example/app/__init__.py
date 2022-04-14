5911dbf933dc6ff52c5c6d207e055808d6202fa1
Mark Gregson <mark.gregson@anu.edu.au>
2022-04-14 16:48:59 +1000

    Don't use default_app_config for Django 3.2+

--- example/app/__init__.py
+++ example/app/__init__.py
@@ -1,1 +1,5 @@
-default_app_config = 'app.apps.AppAppConfig'
+from django import VERSION
+
+# default_app_config was deprecated in Django 3.2.
+if VERSION < (3, 2):
+    default_app_config = 'app.apps.AppAppConfig'
--- multiselectfield/__init__.py
+++ multiselectfield/__init__.py
@@ -1,5 +1,8 @@
+from django import VERSION
+
 from multiselectfield.db.fields import MultiSelectField  # noqa: F401
 from multiselectfield.forms.fields import MultiSelectFormField  # noqa: F401
 
-
-default_app_config = 'multiselectfield.apps.MultiSelectFieldConfig'
+# default_app_config was deprecated in Django 3.2.
+if VERSION < (3, 2):
+    default_app_config = 'multiselectfield.apps.MultiSelectFieldConfig'
